e97afe380bbbedfae7f625b8c95ad3fb6743796b
Josue Carrasco <jos247car@gmail.com>
2020-03-07 16:57:01 -0300

    boton reiniciar funcionando

--- app/app.js
+++ app/app.js
@@ -9,29 +9,32 @@ $(document).ready(function () {
 
         //asigna el nombre del equipo a la variable
         nombre1 = $('#nombre-equipo-1').val();
-        nombre2 = $('#nombre-equipo-2').val();
-        console.log(nombre1, nombre2);
+        nombre2 = $('#nombre-equipo-2').val();      
 
         //asignar nombres a pantalla 2
         $('#titulo-1').text(nombre1);
         $('#titulo-2').text(nombre2);
-       
+
 
         //ocultar pantalla 1 y mostrar pantalla 2
         $('#pantalla-1').hide();
         $('#pantalla-2').show();
     })
+
     //PANTALLA 1
-     //seleccion de tipo de juego
-     $('#tipojuego-24').on('click', function(){
+
+    //seleccion de tipo de juego
+    $('#tipojuego-24').on('click', function () {
         seleccionDeJuego(24);
     })
-    $('#tipojuego-30').on('click', function(){
+    $('#tipojuego-30').on('click', function () {
         seleccionDeJuego(30);
     })
+
     //OBTENIENDO NOMBRES DE EQUIPOS
     $('#tipojuego-24').on('click', function () {
     })
+
     //se asegura de que el nombre del equipo cambie 
     $('#nombre-equipo-1').on('click', function () {
         $('#nombre-equipo-1').val('');
@@ -47,50 +50,73 @@ $(document).ready(function () {
     $('#suma-puntos-1').on('click', function () {
         puntos1++;
         sumaPuntos1();
+        pintarPuntos();
     })
+
     $('#suma-puntos-2').on('click', function () {
         puntos2++;
         sumaPuntos2();
     })
 
     function sumaPuntos1() {
-        $('#puntos-actuales-1').text(puntos1);
+        $('#puntos-equipo-1').text(puntos1);
     }
+
     function sumaPuntos2() {
-        $('#puntos-actuales-2').text(puntos2);
+        $('#puntos-equipo-2').text(puntos2);
     }
 
     // RESTAR PUNTOS
     $('#resta-puntos-1').on('click', function () {
         puntos1--;
         restaPuntos1();
+        
     })
+
     $('#resta-puntos-2').on('click', function () {
         puntos2--;
         restaPuntos2();
     })
 
     function restaPuntos1() {
-        $('#puntos-actuales-1').text(puntos1);
+        $('#puntos-equipo-1').text(puntos1);
     }
+
     function restaPuntos2() {
-        $('#puntos-actuales-2').text(puntos2);
+        $('#puntos-equipo-2').text(puntos2);
     }
     //selecciond de modo de juego 
-    function seleccionDeJuego(num){
-        if(num == 30){
+    function seleccionDeJuego(num) {
+        if (num == 30) {
             tipoJuego = 30;
             $('#tipojuego-30').addClass('seleccionado');
             $('#tipojuego-24').removeClass('seleccionado');
             $('#tipo-de-juego').text('A 30');
-        } else if(num == 24){
+        } else if (num == 24) {
             tipoJuego = 24;
             $('#tipojuego-24').addClass('seleccionado');
             $('#tipojuego-30').removeClass('seleccionado');
             $('#tipo-de-juego').text('A 24');
-        } else{
+        } else {
             tipoJuego = 30;
         }
+    }
+    //para reiniciar juego
+    $('#reiniciar-juego').on('click', function(){
+        reiniciarJuego();
+    })
+    function reiniciarJuego() {
+        $('#pantalla-2').hide();
+        $('#pantalla-1').show();
+        puntos1 = 0;
+        puntos2 = 0;
+        nombre1 = 'Equipo 1';
+        nombre2 = 'Equipo 2';
+        tipoJuego;
+    }
+
+    //Pintar puntos con palitos
+    function pintarPuntos(){
         
     }
 })
